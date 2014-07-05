9b8bcfe8a8be879fbef02eafd29b95295b20d412
Anne Berlandi <berlandi.anne@gmail.com>
2014-07-05 11:03:08 +0200

    les FORM

--- Exercices.php
+++ Exercices.php
@@ -1,9 +1,25 @@
-<title> Hello Bussiere </title>
+<html>
+
+<title> Hello world </title>
 
 <body>
 <?php 
 $date = date("l d F");
 $hour = date("G\hi");
-echo "Nous somme le $date et il est $hour"; 
+// Version "
+echo "Nous somme le $date et il est $hour <br>"; 
+// Version ' et concaténation
+echo 'Nous somme le '.$date." et il est ".$hour;
+
+echo 'rempli le formulaire pour des heures de fun !';
 ?>
+
+<form method="post" action="verif.php">
+Nom : <input type="text" name ="nom"><br>
+Prénom : <input type="text" name="prénom"><br>
+<input type="submit" value="OK">
+</form>
+
 </body>
+
+</html>
---
+++ verif.php
@@ -0,0 +1,16 @@
+<html>
+<?php 
+$nom = $_POST['nom'];
+$prenom = $_POST['prénom'];
+?>
+
+<title> <?php 
+echo 'salut '.$prenom.' !'; ?>
+</title
+
+<body>
+<?php 
+echo 'tu t\'appelles '.$prenom.' '.$nom.'<br>';
+?>
+</body>
+</html>
